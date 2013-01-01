62937394b5e78324794fa63779292339fa5a1851
Tancrède Fourmaintraux <t4ncr3d3@gmail.com>
2013-01-01 14:55:09 -0500

    removing c9 files from repo
    	deleted:    .c9revisions/app.js.c9save
    	deleted:    .c9revisions/public/js/app.js.c9save
    	deleted:    .c9revisions/public/js/bootstrap-angular.js.c9save
    	deleted:    .c9revisions/public/js/controllers.js.c9save
    	deleted:    .c9revisions/public/js/directives.js.c9save
    	deleted:    .c9revisions/public/js/samples/directives-2.js.c9save
    	deleted:    .c9revisions/public/js/samples/directives-3.js.c9save
    	deleted:    .c9revisions/public/js/samples/directives-4.js.c9save
    	deleted:    .c9revisions/public/js/samples/directives-5.js.c9save
    	deleted:    .c9revisions/public/js/services.js.c9save
    	deleted:    .c9revisions/public/lib/angular/angular.js.c9save
    	deleted:    .c9revisions/public/lib/bootstrap.js.c9save
    	deleted:    .c9revisions/routes/api.js.c9save
    	deleted:    .c9revisions/routes/index.js.c9save
    	deleted:    .c9revisions/views/index.jade.c9save
    	deleted:    .c9revisions/views/layout.jade.c9save

---
+++ routes/api/exp.js
@@ -0,0 +1,504 @@
+{
+    "item": {
+        "label": "root"
+    },
+    "split": "h",
+    "childNodes": [{
+        "item": {
+            "label": "courses"
+        },
+        "weight": 7,
+        "split": "v",
+        "childNodes": [{
+            "item": {
+                "label": "samedi"
+            },
+            "weight": 7,
+            "split": "h",
+            "childNodes": [{
+                "item": {
+                    "label": "poulet"
+                },
+                "weight": 7,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#b4fff8"
+            }, {
+                "item": {
+                    "label": "legumes"
+                },
+                "weight": 7,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#b4f8ff"
+            }, {
+                "item": {
+                    "label": "chocolat"
+                },
+                "weight": 7,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#b4dcff"
+            }, {
+                "item": {
+                    "label": "yaourts"
+                },
+                "weight": 7,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#b6b4ff"
+            }],
+            "childrenWeight": 28,
+            "relatWeight": "50.0%",
+            "bgcolor": "#8be9ff"
+        }, {
+            "item": {
+                "label": "dimanche"
+            },
+            "weight": 7,
+            "split": "h",
+            "childNodes": [{
+                "item": {
+                    "label": "boeuf"
+                },
+                "weight": 7,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#b4ceff"
+            }, {
+                "item": {
+                    "label": "fruits"
+                },
+                "weight": 7,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#b4bfff"
+            }, {
+                "item": {
+                    "label": "biscuits"
+                },
+                "weight": 7,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#c4b4ff"
+            }, {
+                "item": {
+                    "label": "perrier"
+                },
+                "weight": 7,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#efb4ff"
+            }],
+            "childrenWeight": 28,
+            "relatWeight": "50.0%",
+            "bgcolor": "#8b92ff"
+        }],
+        "childrenWeight": 70,
+        "relatWeight": "26.0%",
+        "bgcolor": "#4d79ff"
+    }, {
+        "item": {
+            "label": "boulot"
+        },
+        "weight": 17,
+        "split": "v",
+        "childNodes": [{
+            "item": {
+                "label": "Work Bandits"
+            },
+            "weight": 12,
+            "split": "h",
+            "childNodes": [{
+                "item": {
+                    "label": "Business Plan"
+                },
+                "weight": 12,
+                "split": "v",
+                "childNodes": [{
+                    "item": {
+                        "label": "slide 1"
+                    },
+                    "weight": 4,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "33.3%",
+                    "bgcolor": "#dbceff"
+                }, {
+                    "item": {
+                        "label": "slide 2"
+                    },
+                    "weight": 1,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "8.3%",
+                    "bgcolor": "#e0ceff"
+                }, {
+                    "item": {
+                        "label": "slide 3"
+                    },
+                    "weight": 2,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "16.7%",
+                    "bgcolor": "#e9ceff"
+                }, {
+                    "item": {
+                        "label": "slide 4"
+                    },
+                    "weight": 5,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "41.7%",
+                    "bgcolor": "#f7ceff"
+                }],
+                "childrenWeight": 12,
+                "relatWeight": "40.7%",
+                "bgcolor": "#d2b4ff"
+            }, {
+                "item": {
+                    "label": "Financial Forecast"
+                },
+                "weight": 12,
+                "split": "v",
+                "childNodes": [{
+                    "item": {
+                        "label": "slide 1"
+                    },
+                    "weight": 4,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "17.4%",
+                    "bgcolor": "#edceff"
+                }, {
+                    "item": {
+                        "label": "slide 2"
+                    },
+                    "weight": 1,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "4.3%",
+                    "bgcolor": "#f0ceff"
+                }, {
+                    "item": {
+                        "label": "slide 3"
+                    },
+                    "weight": 2,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "8.7%",
+                    "bgcolor": "#f7ceff"
+                }, {
+                    "item": {
+                        "label": "slide 4"
+                    },
+                    "weight": 5,
+                    "childNodes": [{
+                        "item": {
+                            "label": "slide 1"
+                        },
+                        "weight": 2,
+                        "split": "h",
+                        "childNodes": [],
+                        "childrenWeight": 0,
+                        "relatWeight": "50.0%",
+                        "bgcolor": "#ffdfff"
+                    }, {
+                        "item": {
+                            "label": "slide 2"
+                        },
+                        "weight": 2,
+                        "split": "h",
+                        "childNodes": [],
+                        "childrenWeight": 0,
+                        "relatWeight": "50.0%",
+                        "bgcolor": "#ffdffe"
+                    }],
+                    "childrenWeight": 4,
+                    "relatWeight": "39.1%",
+                    "bgcolor": "#ffcefe"
+                }, {
+                    "item": {
+                        "label": "slide 5"
+                    },
+                    "weight": 2,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "8.7%",
+                    "bgcolor": "#ffcef2"
+                }, {
+                    "item": {
+                        "label": "slide 6"
+                    },
+                    "weight": 5,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "21.7%",
+                    "bgcolor": "#ffcee3"
+                }],
+                "childrenWeight": 23,
+                "relatWeight": "59.3%",
+                "bgcolor": "#efb4ff"
+            }],
+            "childrenWeight": 59,
+            "relatWeight": "61.2%",
+            "bgcolor": "#db8bff"
+        }, {
+            "label": "White CRM",
+            "item": {
+                "label": "slide 6"
+            },
+            "weight": 5,
+            "split": "h",
+            "childNodes": [{
+                "item": {
+                    "label": "Marketing Plan"
+                },
+                "weight": 4,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "10.0%",
+                "bgcolor": "#ffb4f3"
+            }, {
+                "item": {
+                    "label": "Dev Plan"
+                },
+                "weight": 4,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "10.0%",
+                "bgcolor": "#ffb4e8"
+            }, {
+                "item": {
+                    "label": "Strat Plan"
+                },
+                "weight": 4,
+                "split": "v",
+                "childNodes": [{
+                    "item": {
+                        "label": "Plan 1"
+                    },
+                    "weight": 4,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "50.0%",
+                    "bgcolor": "#ffcee4"
+                }, {
+                    "item": {
+                        "label": "Plan 2"
+                    },
+                    "weight": 4,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "50.0%",
+                    "bgcolor": "#ffcee0"
+                }],
+                "childrenWeight": 8,
+                "relatWeight": "30.0%",
+                "bgcolor": "#ffb4d1"
+            }, {
+                "item": {
+                    "label": "Job Plan"
+                },
+                "weight": 4,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "10.0%",
+                "bgcolor": "#ffb8b4"
+            }, {
+                "item": {
+                    "label": "Tech Plan"
+                },
+                "weight": 4,
+                "split": "v",
+                "childNodes": [{
+                    "item": {
+                        "label": "Plan 1"
+                    },
+                    "weight": 4,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "33.3%",
+                    "bgcolor": "#ffebce"
+                }, {
+                    "item": {
+                        "label": "Plan 2"
+                    },
+                    "weight": 4,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "33.3%",
+                    "bgcolor": "#ffeece"
+                }, {
+                    "item": {
+                        "label": "Plan 3"
+                    },
+                    "weight": 4,
+                    "childNodes": [],
+                    "childrenWeight": 0,
+                    "relatWeight": "33.3%",
+                    "bgcolor": "#fff3ce"
+                }],
+                "childrenWeight": 12,
+                "relatWeight": "40.0%",
+                "bgcolor": "#ffe5b4"
+            }],
+            "childrenWeight": 40,
+            "relatWeight": "38.8%",
+            "bgcolor": "#ff8bcc"
+        }],
+        "childrenWeight": 116,
+        "relatWeight": "44.9%",
+        "bgcolor": "#ff4dd2"
+    }, {
+        "item": {
+            "label": "City"
+        },
+        "weight": 17,
+        "split": "v",
+        "childNodes": [{
+            "item": {
+                "label": "San Diego"
+            },
+            "weight": 4,
+            "childNodes": [],
+            "childrenWeight": 0,
+            "relatWeight": "11.1%",
+            "bgcolor": "#afff8b"
+        }, {
+            "item": {
+                "label": "Epinay"
+            },
+            "weight": 4,
+            "childNodes": [],
+            "childrenWeight": 0,
+            "relatWeight": "11.1%",
+            "bgcolor": "#8cff8b"
+        }, {
+            "item": {
+                "label": "Paris"
+            },
+            "weight": 4,
+            "split": "h",
+            "childNodes": [{
+                "item": {
+                    "label": "Blomet"
+                },
+                "weight": 4,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#b4ffd7"
+            }, {
+                "item": {
+                    "label": "Tracy"
+                },
+                "weight": 4,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#b4ffdd"
+            }, {
+                "item": {
+                    "label": "Réaumur"
+                },
+                "weight": 4,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#b4ffe8"
+            }, {
+                "item": {
+                    "label": "Vaugirard"
+                },
+                "weight": 4,
+                "childNodes": [],
+                "childrenWeight": 0,
+                "relatWeight": "25.0%",
+                "bgcolor": "#b4fff9"
+            }],
+            "childrenWeight": 16,
+            "relatWeight": "55.6%",
+            "bgcolor": "#8bffcf"
+        }, {
+            "item": {
+                "label": "Londres"
+            },
+            "weight": 4,
+            "childNodes": [],
+            "childrenWeight": 0,
+            "relatWeight": "11.1%",
+            "bgcolor": "#8bc6ff"
+        }, {
+            "item": {
+                "label": "Bangkok"
+            },
+            "weight": 4,
+            "childNodes": [],
+            "childrenWeight": 0,
+            "relatWeight": "11.1%",
+            "bgcolor": "#db8bff"
+        }],
+        "childrenWeight": 36,
+        "relatWeight": "17.9%",
+        "bgcolor": "#4dff79"
+    }, {
+        "item": {
+            "label": "People"
+        },
+        "weight": 17,
+        "split": "v",
+        "childNodes": [{
+            "item": {
+                "label": "John"
+            },
+            "weight": 4,
+            "childNodes": [],
+            "childrenWeight": 0,
+            "relatWeight": "25.0%",
+            "bgcolor": "#ffa18b"
+        }, {
+            "item": {
+                "label": "Kiko"
+            },
+            "weight": 4,
+            "childNodes": [],
+            "childrenWeight": 0,
+            "relatWeight": "25.0%",
+            "bgcolor": "#ffcc8b"
+        }, {
+            "item": {
+                "label": "Inigo"
+            },
+            "weight": 4,
+            "childNodes": [],
+            "childrenWeight": 0,
+            "relatWeight": "25.0%",
+            "bgcolor": "#dbff8b"
+        }, {
+            "item": {
+                "label": "Cuesta"
+            },
+            "weight": 4,
+            "childNodes": [],
+            "childrenWeight": 0,
+            "relatWeight": "25.0%",
+            "bgcolor": "#8bffbe"
+        }],
+        "childrenWeight": 16,
+        "relatWeight": "11.1%",
+        "bgcolor": "#ffd24d"
+    }],
+    "_id": "50e0cd2907de06fa72000001",
+    "weight": 0,
+    "childrenWeight": 296,
+    "relatWeight": "100.0%"
+}
