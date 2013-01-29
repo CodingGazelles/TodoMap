c098484c8990676e5f20e38eadbee6cbab1de63b
tancrede <t4ncr3d3@gmail.com>
2013-01-29 16:33:51 +0100

    saving map to db

--- routes/api/maps.js
+++ routes/api/maps.js
@@ -2,12 +2,16 @@ var mongo = require('mongodb');
 
 var Server = mongo.Server,
     Db = mongo.Db,
-    BSON = mongo.BSONPure;
+    BSON = mongo.BSONPure,
+    ObjectID = mongo.ObjectID;
 
 //mongodb://<user>:<password>@linus.mongohq.com:10013/todomap
 //mongodb://<dbuser>:<dbpassword>@ds047037.mongolab.com:47037/todomap
 
-var server = new Server('ds047037.mongolab.com', 47037, { auto_reconnect: true });
+//var server = new Server('ds047037.mongolab.com', 47037, { auto_reconnect: true });
+//var db = new Db('todomap', server, {native_parser:false});
+
+var server = new Server('localhost', 27017, { auto_reconnect: true });
 var db = new Db('todomap', server, {native_parser:false});
 
 
@@ -15,15 +19,17 @@ db.open(function(err, db) {
     console.log("Connecting to DB");
     if (err) {
         console.log("Connection failed: " + err);
+    } else {
+        console.log("Connecting to DB done");
     }
-    console.log("Connecting to DB done");
     
     db.authenticate( "todoapp", "todoapp", function( err, result){ 
         console.log("Authentication to DB");
         if(err){
             console.log("Authentication failed: " + err);
+        } else {
+            console.log("Authentication to DB done");
         }
-        console.log("Authentication to DB done");
     });
 });
 
@@ -31,7 +37,9 @@ db.open(function(err, db) {
 
 exports.findById = function(req, res) {
     var id = req.params.id;
+    
     console.log('Loading map: ' + id);
+    
     db.collection('maps', function(err, collection) {
         if(err) throw err;
         collection.findOne(
@@ -42,6 +50,7 @@ exports.findById = function(req, res) {
                     res.json( 500, {'message':'An error has occurred while loading the map', 'error': err});
                 } else {
                     console.log('Loading succeed');
+                    // console.log( 'Map: ' + JSON.stringify( item));
                     res.send(item);
                 }
             }
@@ -52,14 +61,17 @@ exports.findById = function(req, res) {
 exports.updateById = function(req, res) {
     var id = req.params.id;
     var map = req.body;
+
+    delete map._id;
+    // map._id = new ObjectID.createFromHexString( map._id);
     
-    console.log( 'Updating map: ' + JSON.stringify( id));
-    console.log( 'Map: ' + JSON.stringify( map));
+    console.log( 'Updating map: ' + id);
+    // console.log( 'Map: ' + JSON.stringify( map));
     
     db.collection('maps', function(err, collection) {
         if(err) throw err;
         collection.update(
-            {'_id':id}, map, {safe:true}, 
+            {'_id': new BSON.ObjectID(id)}, map, {safe:true}, 
             function(err, result) {
                 if (err) {
                     console.log('Updating map err: ' + JSON.stringify( err));
