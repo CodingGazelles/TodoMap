a9023d2ce262cc0543f0896a7beba0c7b124fd2e
tancrede <t4ncr3d3@gmail.com>
2013-02-02 23:41:17 +0100

    Move function colorizeNode to file jmap

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -7,14 +7,14 @@
 todoApp.controller('MapCtrl', function($scope, debounce, maps){
     
     // binding window resize event with $scope.$digest to trigger registered $scope.$watch in map directive
-    angular.element(window).bind('resize', function() {
+    angular.element(window).bind('resize', function onWindowResize() {
         console.log("Callback event window resize");
         debounce( $scope.$digest(), 2000, false);
     });
     
     // listener on saveMap event
-    $scope.$on( 'modifiedMap', function onModifiedMap( event){
-        console.log("callback listener onModifiedMap");
+    $scope.$on( 'saveMap', function( event){
+        console.log("Catch event save map");
         event.stopPropagation();
         $scope.savedState = "Saving ...";
         $scope.$digest();
@@ -27,10 +27,10 @@ todoApp.controller('MapCtrl', function($scope, debounce, maps){
             {id: mapId},
             function(data) { // SUCCESS
                 console.log("call api maps.getMap succeed");
-                var tree = TdNode.parse( data);
-                colorizeNodes([tree]);
+                // console.log("$scope.todoTree: " + JSON.stringify(data));
+                
+                var tree = TdTree.buildTree( data);
                 $scope.todoTree = tree;
-                //console.log("$scope.todoTree: " + JSON.stringify(data));
             },
             function(data) { // FAILURE
                 console.log("call api maps.getMap failed");
@@ -46,9 +46,9 @@ todoApp.controller('MapCtrl', function($scope, debounce, maps){
     
     $scope.saveMap = function(){
         console.log("call function saveMap");
-        //console.log("$scope.mapData: " + JSON.stringify( $scope.mapData));
         
-        var todoData = TdNode.clean( $scope.todoTree);
+        var todoData = TdTree.filterTree( $scope.todoTree);
+        // console.log("$scope.mapData: " + JSON.stringify( todoData));
         
         maps.saveMap(
             {id: todoData._id},
@@ -67,39 +67,12 @@ todoApp.controller('MapCtrl', function($scope, debounce, maps){
     
     console.log("Loading Map");
     $scope.todoTree = {};
-    $scope.loadMap("51051fd25b442a5849000001");
+    $scope.loadMap("510bf39e5ba1aa4c95000001");
+    
 });
 
 
-function colorizeNodes(nodes, parentColor, hueRange) {
-    if (nodes === undefined || nodes.length === 0) return;
 
-    if (parentColor === undefined) parentColor = {
-        h: 0,
-        s: 0.9,
-        v: 1
-    };
-
-    if (hueRange === undefined) hueRange = 360;
-    else hueRange = Math.min(hueRange, 360);
-
-    var hueOffset = hueRange / nodes.length;
-    var color = {};
-    for (var i in nodes) {
-        color.h = (parentColor.h + hueOffset * i) % 360;
-        color.s = parentColor.s;
-        color.v = parentColor.v;
-
-        if (nodes[i].style === undefined) nodes[i].style = {};
-
-        nodes[i].style.bgcolor = tinycolor("hsv (" + color.h + " " + color.s + " " + color.v + ")").toHexString();
-        if (nodes[i].childNodes !== undefined) colorizeNodes(nodes[i].childNodes, {
-            h: color.h,
-            s: color.s * 0.65,
-            v: color.v
-        }, hueRange / nodes.length);
-    }
-}
 
 
 
