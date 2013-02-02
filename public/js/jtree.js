f163b4643a71a9ac8c7737bfabdf1feb9182a652
Tancrède Fourmaintraux <t4ncr3d3@gmail.com>
2013-02-02 17:59:10 -0500

    beautify

--- public/js/jtree.js
+++ public/js/jtree.js
@@ -1,37 +1,31 @@
-
-
-
-function TdNode( data) {
-	_.defaults( this, TdNode.basic, TdNode.extended);
-	
-    this.childNodes = new TdNodeList( this);
-
+function TdNode(data) {
+    _.defaults(this, TdNode.basic, TdNode.extended);
+    this.childNodes = new TdNodeList(this);
     data = data || {};
     var key;
     for (key in data) {
         this[key] = data[key];
     }
-    
-};
+}
 
-function TdNodeList( node) {
-    if( !node) throw new Error("Node can't be null");
+function TdNodeList(node) {
+    if (!node) throw new Error("Node can't be null");
 
     this.node = node;
     this.nodes = [];
-};
+}
 
 // squeleton of a node, contains the persitant properties of a node
 TdNode.basic = {
-    "label":    "",
-    "weight":   0,
-    "opened":   true,
-    "index":     "",
+    "label": "",
+    "weight": 0,
+    "opened": true,
+    "index": "",
     "childNodes": null,
 };
 // squeleton of a node, contains the extended (client side) properties of a node
 TdNode.extended = {
-    "parent":   null,
+    "parent": null,
     "next": null,
     "previous": null,
     "bgcolor": "#ffffff",
@@ -40,121 +34,84 @@ TdNode.extended = {
 };
 
 TdNodeList.prototype = {
-
-    pushNode: function( node) {
-        if (this.length() == 0) {
-        } else {
+    pushNode: function(node) {
+        if (this.length() === 0) {}
+        else {
             this.tail().next = node;
             node.previous = this.tail;
         }
-        this.nodes.push( node);
+        this.nodes.push(node);
         this.indexNodes();
     },
+    insertNode: function(node, index) {
+        if (index < 0) throw new RangeError("Index of node to insert can't be negative");
+        if (index > this.length) throw new RangeError("Index of node to insert can't be higher than list length");
 
-    insertNode: function( node, index){
-        if( index < 0) throw new RangeError("Index of node to insert can't be negative");
-        if( index > this.length) throw new RangeError("Index of node to insert can't be higher than list length");
+        if (index === this.length) {
+            this.pushNode(node);
+        }
+        else {
+            var previous = this.nodes[index - 1],
+                next = this.nodes[index];
 
-        if ( index === this.length) {
-            this.pushNode( node);
-        } else {
-            var previous = this.nodes[ index - 1],
-                next = this.nodes[ index];
-
-            if( previous){
+            if (previous) {
                 previous.next = node;
                 node.previous = previous;
-            };
+            }
 
-            if( next) {
+            if (next) {
                 next.previous = node;
                 node.next = next;
-            };
-            this.nodes.splice( index, 0, node);
+            }
+            this.nodes.splice(index, 0, node);
             this.indexNodes();
-        };
+        }
     },
-    deleteNode: function( index){
-        if( index < 0) throw new RangeError("Index of node to delete can't be negative");
-        if( index > this.length) throw new RangeError("Index of node to delete can't be higher than list length");
+    deleteNode: function(index) {
+        if (index < 0) throw new RangeError("Index of node to delete can't be negative");
+        if (index > this.length) throw new RangeError("Index of node to delete can't be higher than list length");
 
-        var previous = this.nodes[ index - 1],
-            next = this.nodes[ index];
+        var previous = this.nodes[index - 1],
+            next = this.nodes[index];
 
-        if( previous && next){
+        if (previous && next) {
             previous.next = next;
             next.previous = previous;
-        }else if( previous){
+        }
+        else if (previous) {
             previous.next = null;
-        }else if( next){
+        }
+        else if (next) {
             next.previous = null;
-        };
-        this.nodes.splice( index, 1);
+        }
+        this.nodes.splice(index, 1);
         this.indexNodes();
     },
-    moveNode: function( node, index){
+    moveNode: function(node, index) {
 
     },
-    indexNodes: function(){
-        this.nodes.forEach( function( node, index){
+    indexNodes: function() {
+        this.nodes.forEach(function(node, index) {
             node.index = index;
-        })
+        });
     },
-    getNode: function(index) {        
+    getNode: function(index) {
         return this.nodes[index];
     },
-    length: function(){
+    length: function() {
         return this.nodes.length;
     },
-    head: function(){
+    head: function() {
         return this.nodes[0];
     },
-    tail: function(){
+    tail: function() {
         return this.nodes.last;
     }
-
-    // ,
-    
-    // remove: function(index) {
-    //     if ( index > this.Length - 1 || index < 0 ) {
-    //         return null;
-    //     }
-        
-    //     var node = this.head;
-    //     var i = 0;
-        
-    //     if (index == 0) {
-    //         this.head = node.next;
-            
-    //         // check if we removed the only one in the list
-    //         if (this.head == null) {
-    //             this.tail = null;
-    //         }
-    //         else {
-    //             this.head.previous = null;
-    //         }
-    //     }
-    //     else if (index == this.length - 1) {
-    //         node = this.tail;
-    //         this.tail = node.previous;
-    //         this.tail.next = null;
-    //     }
-    //     else {
-    //         while (i++ < index) {
-    //             node = node.next;
-    //         }
-            
-    //         node.previous.next = node.next;
-    //         node.next.previous = node.previous;
-    //     }
-        
-    //     this.length--;
-    // }
 };
 
 TdNode.prototype = {
-    path: function(){
-        return this.parent ? this.parent.path() + "/" + this.index :  "/" + this.index;
+    path: function() {
+        return this.parent ? this.parent.path() + "/" + this.index : "/" + this.index;
     },
     isRoot: function() {
         return this.parent === null;
@@ -162,28 +119,28 @@ TdNode.prototype = {
     isTerminal: function() {
         return this.childNodes.length === 0;
     },
-    nodes: function(){
+    nodes: function() {
         return this.childNodes.nodes;
     },
-    insertChild: function( node, at){
+    insertChild: function(node, at) {
         node.parent = this;
-        this.childNodes.insertNode( node, at);
+        this.childNodes.insertNode(node, at);
         this.rebaseWeight();
     },
-    deleteChild: function( at){
-        this.childNodes.deleteNode( at);
+    deleteChild: function(at) {
+        this.childNodes.deleteNode(at);
         this.rebaseWeight();
     },
-    createSibling: function(){
+    createSibling: function() {
         var newNode = new TdNode();
         newNode.weight = this.weight;
-        this.parent.insertChild( newNode, this.index + 1);
+        this.parent.insertChild(newNode, this.index + 1);
         return newNode;
     },
-    delete: function(){
-        this.parent.deleteChild( this.index);
+    delete: function() {
+        this.parent.deleteChild(this.index);
     },
-    rebaseWeight: function(){
+    rebaseWeight: function() {
         var totalWeight = 0;
         this.nodes().forEach(function(node) {
             totalWeight += node.weight;
@@ -192,41 +149,18 @@ TdNode.prototype = {
             node.weight = node.weight * 100 / totalWeight;
         });
     },
-    focus: function(){
-        if( this.labelElement){
+    focus: function() {
+        if (this.labelElement) {
             this.labelElement[0].focus();
         }
     },
-    toString: function(){
+    toString: function() {
         return "[TdNode " + this.path() + ", " + this.label + "]";
     }
-
-    // addChild: function(){
-    //     var newNode = new TdNode( this),
-    //      lastNode = this.childNodes.last,
-    //      length = this.childNodes.length,
-    //      newTotalWeight;
-        
-    //     newNode.path = this.path + "/" + length;
-    //     this.childNodes.push(newNode);
-        
-    //     if( lastNode){
-    //      newNode.weight = lastNode.weight;
-    //      newTotalWeight = 100 + newNode.weight;
-    //     } else {
-    //      newNode.weight = newTotalWeight = 100;
-    //     }
-        
-    //     this.childNodes.forEach(function(node) {
-    //         node.weight = node.weight / newTotalWeight;
-    //     });
-        
-    //     return newNode;
-    // },
 };
 
 // Build an extended node tree
-var TdTree = function(){};
+var TdTree = function() {};
 
 TdTree.buildTree = function(data) {
     var root;
@@ -237,18 +171,19 @@ TdTree.buildTree = function(data) {
         }
         return nodeData;
     };
-    
+
     (function walkDown(node, parent) {
         var newNode;
-        
+
         if (!parent) {
             newNode = root = new TdNode(getNodeData(node));
-        } else {
-            var newNode = new TdNode(getNodeData(node));
+        }
+        else {
+            newNode = new TdNode(getNodeData(node));
             newNode.parent = parent;
             parent.childNodes.pushNode(newNode);
         }
-        
+
         if ("childNodes" in node) {
             for (var i = 0; i < node.childNodes.length; i++) {
                 walkDown(node.childNodes[i], newNode);
@@ -260,21 +195,22 @@ TdTree.buildTree = function(data) {
 };
 
 // remove non persistent properties from the node tree
-TdTree.filterTree = function(data){
+TdTree.filterTree = function(data) {
     var root;
 
     (function walkDown(node, parent) {
         var newNode;
-        
+
         if (!parent) {
-            newNode = root = _.pick( node, _.keys( TdNode.basic), "_id");
+            newNode = root = _.pick(node, _.keys(TdNode.basic), "_id");
             newNode.childNodes = [];
-        } else {
-            newNode = _.pick( node, _.keys( TdNode.basic));
-            newNode.childNodes = [];
-            parent.childNodes.push( newNode);
         }
-        
+        else {
+            newNode = _.pick(node, _.keys(TdNode.basic));
+            newNode.childNodes = [];
+            parent.childNodes.push(newNode);
+        }
+
         if ("childNodes" in node) {
             for (var i = 0; i < node.nodes().length; i++) {
                 walkDown(node.nodes()[i], newNode);
