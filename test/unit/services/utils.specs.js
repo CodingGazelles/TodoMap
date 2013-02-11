ba0b79b97b36567a46f49a20db711c1a25028034
tancrede <t4ncr3d3@gmail.com>
2013-02-11 12:55:09 +0100

    Add specs for debounce service

--- test/unit/services/utils.specs.js
+++ test/unit/services/utils.specs.js
@@ -1,41 +1,54 @@
 //
 // test/unit/services/servicesSpec.js
 //
-describe("Unit: Testing utils ", function() {
+describe("Unit : ", function() {
 
     beforeEach(angular.mock.module('App'));
 
-    /* $eventManager Services */
-
-    describe(": $debounce ", function() {
+    describe("Testing $debounce : ", function() {
 
         it('should contain a $debounce service', inject(function($debounce) {
             expect($debounce).toBeTruthy();
         }));
 
-        var fn, spy;
+        var fn, ddfn, ddfn, spy, wait;
+        var pause = function(ms) {
+            console.log("Start pause " + ms + " ms");
+            ms += new Date().getTime();
+            while(new Date() < ms) {}
+            console.log("End pause ");
+        };
 
         beforeEach(inject(function($debounce) {
+            wait = 500;
             spy = jasmine.createSpy('debounceSpy');
             fn = function() {
-                $debounce(spy(), 5000, false);
+                console.log("Call fn");
+                spy();
+            };
+            dfn = $debounce(fn, wait, false);
+            ddfn = function() {
+                console.log("Call dfn");
+                dfn();
             };
 
-            fn();
-            fn();
-            fn();
-            fn();
-            fn();
-            fn();
+            ddfn();
+            ddfn();
+            ddfn();
+            ddfn();
+            ddfn();
+            ddfn();
+
+            pause(wait*2);    // pause to let the debounce enought time to trigger fn
         }));
 
-        it("tracks that the spy was called", function() {
+        it("tracks that the spy was called", inject(function($timeout) {
             expect(spy).toHaveBeenCalled();
-        });
+        }));
 
-        it("tracks its number of calls", function() {
+        it("tracks its number of calls", inject(function($timeout) {
             expect(spy.calls.length).toEqual(1);
-        });
+        }));
 
         // todo : complete tests on $eventManager
     });
