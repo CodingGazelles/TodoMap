595feb43360d7b6f9f680c4672be6e743aab6660
tancrede <t4ncr3d3@gmail.com>
2013-03-15 21:53:02 -0300

    Split services.js into different files

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,10 @@ var App = window.App = angular.module( 'App',
 	[
 		'App.Controllers',
 		'App.Filters',
-		'App.Services', 
+		'App.Services.Events',
+		'App.Services.Map',
+		'App.Services.Tree',
+		'App.Services.Storage', 
 		'App.Utils',
 		'App.Directives',
 		'App.Routes'
